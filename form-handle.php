04c07c1e085cbf6a5c0c392447905188616e6e97
AdebisiHammed <adebisidamola75@gmail.com>
2023-03-01 22:46:11 +0100

    University Website

---
+++ form-handle.php
@@ -0,0 +1,36 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'info@skillseekho.org';
+
+
+$email_subject = 'New Form Subimission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User subject: $subject.\n".
+               "User message: $message .\n".;
+
+
+
+
+$to = 'adebisidamola75@gmail.com';
+
+$headers = "From $email_from \r\n";
+
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+
+mail($to, $email_subject, $email_body, $headers );
+
+
+header("Location: contact.html"); 
+
+
+
+?>
